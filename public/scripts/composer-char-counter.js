26523e5ac3c0a90ab832e8d9f4ee0d3dd001a2a1
Monica Li <monicali@live.ca>
2020-11-18 04:47:21 +0000

    add css styles for layout to display "posted" tweets. Fix organization of styles of layout and new-tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  $('textarea').keydown(function() {
+    const value = $(this).val();
+    const remainingChar = 140 - value.length;
+    const counter = $(this).siblings().find('.counter');
+    if (remainingChar < 0) {
+      counter.text(remainingChar).css('color', 'red');
+    } else {
+      counter.text(remainingChar);
+    }
+  })
+});
