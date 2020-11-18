61c3b0b9461b9455fdfaec1e494eab81bb23ff13
Monica Li <monicali@live.ca>
2020-11-18 22:25:45 +0000

    adds dynamic tweets. Creates functions to render a tweet data array

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = {
+const data = [
+  {
   "user": {
     "name": "Newton",
     "avatars": "https://i.imgur.com/73hZDYK.png",
@@ -13,22 +14,41 @@ const tweetData = {
     "text": "If I have seen further it is by standing on the shoulders of giants"
   },
   "created_at": 1461116232227
-}
+  }, 
+  {
+    "user": {
+      "name": "Meow",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@MeowCity"
+    },
+    "content": {
+      "text": "mememememowwmeowmoewmoewmoewmeow!!"
+    },
+    "created_at": 1461113959088
+    }
+]
 
 $(document).ready(function() {
-  const createTweetElement = function (tweetData) {
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+    }
+  }
+
+  const createTweetElement = function (tweet) {
     const $tweet = $(`
       <article>
       <header class="tweet-header">
         <div>
           <span class="material-icons"> pets </span>
-          <span class="username">${tweetData.user.name}</span>
+          <span class="username">${tweet.user.name}</span>
         </div>
-        <span class="tweeter-handle">${tweetData.user.handle}</span>
+        <span class="tweeter-handle">${tweet.user.handle}</span>
       </header>
-      <textarea class="tweeter-content">${tweetData.content.text}</textarea>
+      <textarea class="tweeter-content">${tweet.content.text}</textarea>
       <footer class="tweet-footer">
-        <span>${tweetData.created_at}</span>
+        <span>${tweet.created_at}</span>
         <div class="icons">
           <span class="material-icons"> flag </span>
           <span class="material-icons"> repeat </span>
@@ -37,13 +57,10 @@ $(document).ready(function() {
       </footer>
       </article>
     `);
-
     return $tweet;
   }
-}
-const $tweet = createTweetElement(tweetData);
+  renderTweets(data);
+});
 
-console.log($tweet); // to see what it looks like
 
-$('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
 
