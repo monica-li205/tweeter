b1113ed90440ddcc117e6e8b0d498e3a1f07f682
Monica Li <monicali@live.ca>
2020-11-19 00:14:50 +0000

    adds sumbit button event handler to send a post request to /tweets server. Adds get request to load and render tweets on server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,30 +3,30 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const data = [
-  {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
-  }, 
-  {
-    "user": {
-      "name": "Meow",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@MeowCity"
-    },
-    "content": {
-      "text": "mememememowwmeowmoewmoewmoewmeow!!"
-    },
-    "created_at": 1461113959088
-    }
-]
+// const data = [
+//   {
+//   "user": {
+//     "name": "Newton",
+//     "avatars": "https://i.imgur.com/73hZDYK.png",
+//     "handle": "@SirIsaac"
+//   },
+//   "content": {
+//     "text": "If I have seen further it is by standing on the shoulders of giants"
+//   },
+//   "created_at": 1461116232227
+//   }, 
+//   {
+//     "user": {
+//       "name": "Meow",
+//       "avatars": "https://i.imgur.com/73hZDYK.png",
+//       "handle": "@MeowCity"
+//     },
+//     "content": {
+//       "text": "mememememowwmeowmoewmoewmoewmeow!!"
+//     },
+//     "created_at": 1461113959088
+//     }
+// ]
 
 $(document).ready(function() {
   const renderTweets = function(tweets) {
@@ -59,8 +59,27 @@ $(document).ready(function() {
     `);
     return $tweet;
   }
-  renderTweets(data);
+
+  $('#submit-tweet').submit(function (event) {
+    console.log('Button clicked, performing ajax call...');
+    event.preventDefault();
+    const queryString = $("#tweet-text").serialize();
+    console.log(queryString);
+    $.ajax({
+      type: 'POST',
+      url: '/tweets',
+      data: queryString,
+      success: 'ok',
+      dataType: 'json',
+    });
+    
+  })
+  
+  const loadTweets = function() {
+    $.getJSON('/tweets', function(data) {
+      renderTweets(data);
+    })
+  }
+  loadTweets();
 });
-
-
 
