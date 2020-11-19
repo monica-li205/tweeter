954d9764afe5d4c6a9d4449d305010c3a1192cd0
Monica Li <monicali@live.ca>
2020-11-19 02:54:14 +0000

    adds soft refresh when new tweet submits

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,39 +3,8 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-// const data = [
-//   {
-//   "user": {
-//     "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//     "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-//   }, 
-//   {
-//     "user": {
-//       "name": "Meow",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//       "handle": "@MeowCity"
-//     },
-//     "content": {
-//       "text": "mememememowwmeowmoewmoewmoewmeow!!"
-//     },
-//     "created_at": 1461113959088
-//     }
-// ]
 
 $(document).ready(function() {
-  const renderTweets = function(tweets) {
-    for (let tweet of tweets) {
-      const $tweet = createTweetElement(tweet);
-      $('#tweet-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-    }
-  }
-
   const createTweetElement = function (tweet) {
     const $tweet = $(`
       <article>
@@ -56,39 +25,56 @@ $(document).ready(function() {
         </div>
       </footer>
       </article>
-    `);
+    `)
     return $tweet;
   }
 
-  $('#submit-tweet').submit(function (event) {
-    console.log('Button clicked, performing ajax call...');
-    event.preventDefault();
-    const queryString = $("#tweet-text").serialize();
-    console.log(queryString);
-
-    if ($('#tweet-text').val() === '') {
-      return window.alert('cannot submit empty tweet');
-    } else if ($('#tweet-text').val() === null) {
-      return window.alert('tweet is invalid');
-    } else if ($('#tweet-text').val().length > 140)  {
-      return window.alert('tweet exceeds permitted character count');
-    } else {
-      $.ajax({
-        type: 'POST',
-        url: '/tweets',
-        data: queryString,
-        success: 'ok',
-        dataType: 'json',
-      });
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      const $tweet = createTweetElement(tweet);
+      $('#tweet-container').prepend($tweet);
     }
-    
-  })
+  }
   
   const loadTweets = function() {
     $.getJSON('/tweets', function(data) {
       renderTweets(data);
     })
   }
+
   loadTweets();
+
+  $('#submit-tweet').submit(function (event) {
+
+    console.log('Button clicked, performing ajax call...');
+    event.preventDefault();
+    const queryString = $("#tweet-text").serialize();
+    console.log(queryString);
+    
+    if ($('#tweet-text').val() === '') {
+      return window.alert('cannot submit empty tweet');
+
+    } else if ($('#tweet-text').val() === null) {
+      return window.alert('tweet is invalid');
+
+    } else if ($('#tweet-text').val().length > 140)  {
+      return window.alert('tweet exceeds permitted character count');
+
+    } else {
+      // $.post('/tweets', queryString)
+      $.ajax({
+        type: 'POST',
+        url: '/tweets',
+        data: queryString,
+        success: 'ok',
+      })
+
+      .then(() => {
+        $('#tweet-text').val('')
+        $('.counter').val('140')
+        loadTweets()
+      })
+    }
+  })
 });
 
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
     const value = $(this).val();
     const remainingChar = 140 - value.length;
     const counter = $(this).siblings().find('.counter');
+    
     if (remainingChar < 0) {
       counter.text(remainingChar).css('color', 'red');
     } else {
